1d2c4213fd6721e672d4bc837eb52c10ecd5b43f
girijabhagwat10 <girija.bhagwat@primartis.com>
2020-07-24 15:49:20 -0400

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "notes-app-gsb"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://ziwr9enpek.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_87g21kUQy",
+    APP_CLIENT_ID: "7a2epbo6213esegol0lqk5s22a",
+    IDENTITY_POOL_ID: "us-east-2:d4abd76e-7114-435e-abe1-d702e5534da9"
+  }
+};
